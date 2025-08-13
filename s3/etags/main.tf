ec692c06799c195a66146c77901a0eee60841d01
Red-DBG <138537117+Red-DBG@users.noreply.github.com>
2025-08-13 13:40:50 +0800

    etag example

---
+++ s3/etags/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.8.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object"{
+    bucket = resource.aws_s3_bucket.default.bucket
+    key = "myfile.txt"
+    source = "myfile.txt"
+    etag = filemd5("myfile.txt")
+}
